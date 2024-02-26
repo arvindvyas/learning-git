011c4f04f7f333ab91bfc257c8664101a6fdbc97
Aman Mishra <you@example.com>
2024-02-26 18:06:32 +0530

    created programs to understand error handling and send keyword to access private method

---
+++ Aman Mishra/26 feb 2024/error_handling.rb
@@ -0,0 +1,13 @@
+def learn_error_handling
+    begin
+        puts "before error"
+        # raise 'This is an error'
+        
+        puts "after error"
+        rescue
+            puts 'you are rescued'
+        
+    end
+    puts "after begin"
+end
+learn_error_handling
